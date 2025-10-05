c9d5c5214cbaeb7dae828323d129b55b68d380a5
daniilt23 <tovstukhadaniil@gmail.com>
2025-10-05 17:37:08 +0300

    add ex_9

---
+++ ex_9/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var s string
+	fmt.Scanf("%s", &s)
+	answer, err := checkCondition(s)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(answer)
+}
+
+func checkCondition(s string) (string, error) {
+	if len(s) == 0 {
+		return "", nil
+	}
+	sSplit := strings.Split(s, "")
+	count := 0
+	for _, val := range sSplit {
+		if isDigit(val) {
+			count++
+		}
+	}
+
+	if count == len(s) {
+		return "", errors.New("строка не может состоять только из цифр")
+	}
+
+	return strings.Join(stringUnpacking(sSplit), ""), nil
+}
+
+func stringUnpacking(sSplit []string) []string {
+	answer := []string{}
+	answer = append(answer, sSplit[0])
+	for i := 1; i < len(sSplit); i++ {
+		if sSplit[i] == "\\" {
+			i++
+			answer = append(answer, sSplit[i])
+			continue
+		}
+		if isDigit(sSplit[i]) {
+			num, _ := strconv.Atoi(sSplit[i])
+			if isDigit(sSplit[i-1]) {
+				num++
+			}
+			pushSlice := strings.Repeat(sSplit[i-1], num-1)
+			answer = append(answer, pushSlice)
+		} else {
+			answer = append(answer, sSplit[i])
+		}
+	}
+
+	return answer
+}
+
+func isDigit(s string) bool {
+	return s >= "0" && s <= "9"
+}
